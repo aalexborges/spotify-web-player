24c8c492c044013a02b3f8441a2f9d9a26a62359
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-07 15:14:50 -0300

    Created testUtils.tsx

--- tests/utils/testUtils.tsx
+++ tests/utils/testUtils.tsx
@@ -1,14 +1,20 @@
 import { cleanup, render } from '@testing-library/react'
+import { ReactNode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { afterEach } from 'vitest'
 
 afterEach(() => {
   cleanup()
 })
 
+const AllTheProviders = ({ children }: { children: ReactNode }) => {
+  return <BrowserRouter>{children}</BrowserRouter>
+}
+
 const customRender = (ui: React.ReactElement, options = {}) =>
   render(ui, {
     // wrap provider(s) here if needed
-    wrapper: ({ children }) => children,
+    wrapper: AllTheProviders,
     ...options,
   })
 
