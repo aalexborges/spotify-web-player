3edb68740a9e9f0e270ab368a52a0efe2a40dd7b
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-07 18:15:59 -0300

    Changed App screen

--- src/screens/App/index.tsx
+++ src/screens/App/index.tsx
@@ -1,11 +1,36 @@
-import AsideNavigation from '../../components/AsideNavigation'
+import { Route, Routes } from 'react-router-dom'
 
-import { GridContainer } from './styles'
+import AsideNavigation from '../../components/AsideNavigation'
+import MainBGGradient from '../../components/MainBGGradient'
+import NavigationBar from '../../components/NavigationBar'
+
+// Screens
+import Collection from '../Collection'
+import Home from '../Home'
+import LikedSongs from '../LikedSongs'
+import Search from '../Search'
+
+import { Content, GridContainer, Main } from './styles'
 
 const App = () => {
   return (
     <GridContainer>
       <AsideNavigation />
+
+      <Main>
+        <MainBGGradient />
+
+        <Content>
+          <NavigationBar />
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/collection" element={<Collection />} />
+            <Route path="/collection/tracks" element={<LikedSongs />} />
+          </Routes>
+        </Content>
+      </Main>
     </GridContainer>
   )
 }
--- src/screens/App/styles.ts
+++ src/screens/App/styles.ts
@@ -6,5 +6,16 @@ export const GridContainer = styled.div`
 
   display: grid;
   grid-template-columns: 241px 1fr;
-  grid-template-rows: 100%;
+  grid-template-rows: 1fr auto;
+`
+
+export const Main = styled.main`
+  position: relative;
+  background-color: #121212;
+`
+
+export const Content = styled.div`
+  width: 100%;
+  z-index: 1;
+  position: relative;
 `
