7b3c3c41b0964058db8402d405467de91153fea5
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-09 15:11:41 -0300

    Changed App screen - NavigationBar component changed bg

--- src/screens/App/index.tsx
+++ src/screens/App/index.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useRef } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import AsideNavigation from '../../components/AsideNavigation'
@@ -13,15 +14,27 @@ import Search from '../Search'
 import { Content, GridContainer, Main } from './styles'
 
 const App = () => {
+  const navigationRef = useRef<HTMLElement>(null)
+
+  const onMainScroll = useCallback((e: React.UIEvent<HTMLElement, UIEvent>) => {
+    const navigation = navigationRef.current
+    if (!navigation) return
+
+    const y = e.currentTarget.scrollTop
+    const opacity = y >= 100 ? 1 : y / 100
+
+    navigation.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`
+  }, [])
+
   return (
     <GridContainer>
       <AsideNavigation />
 
-      <Main>
+      <Main onScroll={onMainScroll}>
         <MainBGGradient />
 
         <Content>
-          <NavigationBar />
+          <NavigationBar ref={navigationRef} />
 
           <Routes>
             <Route path="/" element={<Home />} />
