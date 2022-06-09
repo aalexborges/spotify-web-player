f3fb9d8fb17007b2b286ba1ae63bde66454ce2cd
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-09 10:38:17 -0300

    Updated MainBGGradient component

--- src/components/MainBGGradient/index.tsx
+++ src/components/MainBGGradient/index.tsx
@@ -1,6 +1,12 @@
+import { useLocation } from 'react-router-dom'
+
 import { BGGradient } from './styles'
 
 const MainBGGradient = () => {
+  const { pathname } = useLocation()
+
+  if (pathname !== '/') return null
+
   return <BGGradient />
 }
 
