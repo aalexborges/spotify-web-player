cfa5dc7a58be5640c1cfa22629bdb2142516a068
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-14 16:05:31 -0300

    Updated to LikedSongs screen

--- src/screens/LikedSongs/index.tsx
+++ src/screens/LikedSongs/index.tsx
@@ -1,5 +1,29 @@
+import { Container } from './styles'
+
+import PlaylistHeader from '../../components/PlaylistHeader'
+import PlaylistController from '../../components/PlaylistController'
+import PlaylistGradientBG from '../../components/PlaylistGradientBG'
+
+import user from '../../assets/svg/user.svg'
+import likedSongsImg from '../../assets/images/liked-songs.png'
+
+import likedSongs from '../../data/likedSongs'
+
 const LikedSongs = () => {
-  return <section></section>
+  return (
+    <Container>
+      <PlaylistHeader
+        src={likedSongsImg}
+        title="Músicas Curtidas"
+        author={{ name: 'Fake User', src: user }}
+        likedSongs
+        numberOfSongs={likedSongs.length}
+      />
+
+      <PlaylistGradientBG likedSongs />
+      <PlaylistController />
+    </Container>
+  )
 }
 
 export default LikedSongs
