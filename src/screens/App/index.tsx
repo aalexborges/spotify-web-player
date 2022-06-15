90bad11b256b2a8caa370f3ac0e03a0062acb831
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-15 19:23:40 -0300

    Updated the App screen

--- src/screens/App/index.tsx
+++ src/screens/App/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import { Scrollbars } from 'react-custom-scrollbars'
 
@@ -16,25 +16,46 @@ import Search from '../Search'
 import { Content, GridContainer, Main, Scroll, ScrollThumb } from './styles'
 
 const App = () => {
-  useLocation()
+  const { pathname } = useLocation()
 
+  const scrollRef = useRef<Scrollbars>(null)
   const navigationRef = useRef<HTMLElement>(null)
+  const playlistHeaderRef = useRef<HTMLDivElement>(null)
 
-  const onMainScroll = useCallback((e: React.UIEvent<HTMLElement, UIEvent>) => {
-    const navigation = navigationRef.current
-    if (!navigation) return
+  const onMainScroll = useCallback(
+    (e: React.UIEvent<HTMLElement, UIEvent>) => {
+      const navigation = navigationRef.current
+      const playlistHeader = playlistHeaderRef.current
+      const y = e.currentTarget.scrollTop
 
-    const y = e.currentTarget.scrollTop
-    const opacity = y >= 100 ? 1 : y / 100
+      if (navigation && pathname !== '/collection/tracks') {
+        const opacity = y >= 100 ? 1 : y / 100
 
-    navigation.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`
-  }, [])
+        navigation.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`
+        navigation.classList.remove('likedSongs')
+      } else if (navigation) {
+        navigation.style.backgroundColor = ''
+
+        if (y >= 280) navigation.classList.add('likedSongs')
+        else navigation.classList.remove('likedSongs')
+      }
+
+      if (y >= 379) playlistHeader?.classList.add('bg')
+      else playlistHeader?.classList.remove('bg')
+    },
+    [pathname]
+  )
+
+  useEffect(() => {
+    scrollRef.current?.scrollTop(0)
+  }, [pathname])
 
   return (
     <GridContainer>
       <AsideNavigation />
 
       <Scrollbars
+        ref={scrollRef}
         onScroll={onMainScroll}
         style={{ backgroundColor: '#121212' }}
         renderTrackVertical={props => <ScrollThumb {...props} />}
@@ -49,7 +70,10 @@ const App = () => {
               <Route path="/" element={<Home />} />
               <Route path="/search" element={<Search />} />
               <Route path="/collection" element={<Collection />} />
-              <Route path="/collection/tracks" element={<LikedSongs />} />
+              <Route
+                path="/collection/tracks"
+                element={<LikedSongs ref={playlistHeaderRef} />}
+              />
             </Routes>
           </Content>
         </Main>
