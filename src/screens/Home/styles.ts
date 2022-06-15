6ea810fcb7e9541391b89e099997cce626d2cb26
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-15 16:50:30 -0300

    Fixex Home screen style

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -36,27 +36,59 @@ export const RowList = styled.div`
 
   grid-template-columns: repeat(2, minmax(0px, 1fr));
 
+  & > div {
+    display: none;
+  }
+
+  & > div:nth-child(-n + 2) {
+    display: block;
+  }
+
   @media (min-width: 773px) {
     grid-template-columns: repeat(3, minmax(0px, 1fr));
+
+    & > div:nth-child(-n + 3) {
+      display: block;
+    }
   }
 
   @media (min-width: 973px) {
     grid-template-columns: repeat(4, minmax(0px, 1fr));
+
+    & > div:nth-child(-n + 4) {
+      display: block;
+    }
   }
 
   @media (min-width: 1301px) {
     grid-template-columns: repeat(5, minmax(0px, 1fr));
+
+    & > div:nth-child(-n + 5) {
+      display: block;
+    }
   }
 
   @media (min-width: 1505px) {
     grid-template-columns: repeat(6, minmax(0px, 1fr));
+
+    & > div:nth-child(-n + 6) {
+      display: block;
+    }
   }
 
   @media (min-width: 1709px) {
     grid-template-columns: repeat(7, minmax(0px, 1fr));
+
+    & > div:nth-child(-n + 7) {
+      display: block;
+    }
   }
 
   @media (min-width: 1913px) {
     grid-template-columns: repeat(8, minmax(0px, 1fr));
+
+    & > div {
+      display: block;
+    }
   }
 `
